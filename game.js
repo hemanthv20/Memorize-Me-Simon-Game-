1cd72bc4193086dcb313ecb28ad88b4c5ecb5254
hemanthv20 <144247131+hemanthv20@users.noreply.github.com>
2025-04-01 17:56:54 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,76 +5,70 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-function startGame() {
+// Start the game on first click or touch
+$(document).on("click touchstart", function () {
     if (!started) {
         $("#level-title").text("Level " + level);
         newSequence();
         started = true;
     }
-}
+});
 
-$(document).on("click touchstart", startGame);
-
-$('.btn').click(function() {
+// Handle button clicks
+$('.btn').click(function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
     checkAnswer(userClickedPattern.length - 1);
 });
 
+// Check if user's input matches the game sequence
 function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success");
-
         if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() {
-                newSequence();
-            }, 1000);
+            setTimeout(newSequence, 1000);
         }
     } else {
-        console.log("wrong");
         playSound("wrong");
         $("body").addClass("game-over");
-        setTimeout(function() {
-            $("body").removeClass("game-over");
-        }, 500);
+        setTimeout(() => $("body").removeClass("game-over"), 500);
         $("#level-title").text("Game Over, Click to Restart");
 
-        $(document).one("click touchstart", function() {
+        // Restart game on next click/touch
+        $(document).one("click touchstart", function () {
             startOver();
-            startGame();
+            newSequence();
         });
     }
 }
 
+// Generate a new step in the sequence
 function newSequence() {
     userClickedPattern = [];
     level++;
     $("#level-title").text("Level " + level);
-    
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    
+
+    var randomChosenColour = buttonColours[Math.floor(Math.random() * 4)];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
 
+// Play sound for a given button
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+    new Audio("sounds/" + name + ".mp3").play();
 }
 
+// Add button press animation
 function animatePress(currentColour) {
     $('#' + currentColour).addClass("pressed");
-    setTimeout(function() {
-        $('#' + currentColour).removeClass("pressed");
-    }, 100);
+    setTimeout(() => $('#' + currentColour).removeClass("pressed"), 100);
 }
 
+// Reset the game
 function startOver() {
     level = 0;
     gamePattern = [];
